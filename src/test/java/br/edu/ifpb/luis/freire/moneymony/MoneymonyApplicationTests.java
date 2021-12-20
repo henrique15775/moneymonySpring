a138bf8f61569388f959b28b94817ce5713a4916
Luís Henrique <61325756+henrique15775@users.noreply.github.com>
2021-12-19 23:53:05 -0300

    Add files via upload

---
+++ src/test/java/br/edu/ifpb/luis/freire/moneymony/MoneymonyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.luis.freire.moneymony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
